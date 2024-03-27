cf19a43eb9d945e8470bb5cb2e5359946c5a733b
CLV\manh.lam <lamquangmanh@gmail.com>
2024-03-27 14:40:49 +0700

    Initital

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { UserEntity, PaymentEntity } from './models';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: Number(process.env.POSTGRES_PORT || 5432),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [UserEntity, PaymentEntity],
-      synchronize: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
