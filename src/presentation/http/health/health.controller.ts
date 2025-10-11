727572f730114323536960813ac3b0fdad029705
manh-lam-tpv-clv <manh.lam.tpv@one-line.com>
2025-10-11 11:11:04 +0700

    feat: add health check

--- src/presentation/http/health/health.controller.ts
+++ src/presentation/http/health/health.controller.ts
@@ -1,10 +1,21 @@
 // import from libraries
 import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 
-@Controller('/health')
+@Controller('/healthz')
 export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly db: TypeOrmHealthIndicator,
+  ) {}
+
   @Get()
-  healthCheck(): { status: string } {
-    return { status: 'ok' };
+  @HealthCheck()
+  healthCheck() {
+    return this.health.check([async () => this.db.pingCheck('database')]);
   }
 }
